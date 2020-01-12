c8987a9a8e897e7acda3f82ae29aca086434cba9
Daniel Gomez <dggomez21@gmail.com>
2020-01-12 10:32:38 -0500

    store password function

--- 100days/passwordStore.py
+++ 100days/passwordStore.py
@@ -1,3 +1,11 @@
+from pymongo import MongoClient
+
 # get website, username, and password
 def storing(website, username, password):
-# send to db
+    # send to db
+    post = {"website": website, "username": username, "password": password}
+    client = MongoClient(os.environ['URI'])
+    db = client.passwords
+    # collection = db['passwords']
+    db.posts.insert_one(post).inserted_id
+    client.close()
