28478e39e446b237db65070ce7417ccd400dc618
Daniel Gomez <dggomez21@gmail.com>
2020-01-22 23:02:06 -0500

    change calc

--- 100days/tipCal.py
+++ 100days/tipCal.py
@@ -1,7 +1,11 @@
 val = float(input("Price: "))
+pay = float(input("Payment: "))
 rate = int(input("Rate your expierience 1 to 10: "))
 
 rates = [1, 1.05, 1.05, 1.10 , 1.10, 1.15, 1.15, 1.20, 1.20, 1.25]
-print("Pay: "+str(round(val *rates[rate-1], 2)))
-print("Rate: "+ str(rates[rate-1]))
+p = round(val *rates[rate-1], 2)
+r = rates[rate-1]
+print("Pay: "+str(p))
+print("Rate: "+ str(r))
 print("Tip: "+str(round(val * (rates[rate-1]-1), 2)))
+print("Change: "+ str(round(pay-p,2)))
